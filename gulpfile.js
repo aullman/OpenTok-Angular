c3d3832156b0fd68539eb8e219b6861ca3b2d25b
Adam Ullman <adam@tokbox.com>
2015-06-17 21:54:58 +1000

    Adding .jshintrc and cleaning up JS

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,19 @@
 var gulp = require('gulp'),
-    jshint = require('gulp-jshint'),
-    rename = require('gulp-rename'),
-    uglify = require('gulp-uglify'),
-    bower = require('gulp-bower');
+  jshint = require('gulp-jshint'),
+  rename = require('gulp-rename'),
+  uglify = require('gulp-uglify'),
+  bower = require('gulp-bower');
 
-gulp.task('bower', function () {
-    return bower();
+gulp.task('bower', function() {
+  return bower();
 });
 
-gulp.task('default', ['bower'], function(){
-    gulp.src('opentok-angular.js')
-        .pipe(jshint())
-        .pipe(uglify({preserveComments: "some"}))
-        .pipe(rename('opentok-angular.min.js'))
-        .pipe(gulp.dest('./'));
+gulp.task('default', ['bower'], function() {
+  gulp.src('opentok-angular.js')
+    .pipe(jshint())
+    .pipe(uglify({
+      preserveComments: 'some'
+    }))
+    .pipe(rename('opentok-angular.min.js'))
+    .pipe(gulp.dest('./'));
 });
--- opentok-angular.js
+++ opentok-angular.js
@@ -6,180 +6,188 @@
  *  @Author: Adam Ullman (http://github.com/aullman)
  *  @Copyright (c) 2014 Adam Ullman
  *  @License: Released under the MIT license (http://opensource.org/licenses/MIT)
-**/
+ **/
 
-if (!window.TB) throw new Error("You must include the TB library before the TB_Angular library");
+if (!window.TB) throw new Error('You must include the TB library before the TB_Angular library');
 
-var OpenTokAngular = angular.module('opentok', [])
-.factory("TB", function () {
+angular.module('opentok', [])
+  .factory('TB', function() {
     return TB;
-})
-.factory("OTSession", ['TB', '$rootScope', function (TB, $rootScope) {
-    var OTSession = {
+  })
+  .factory('OTSession', ['TB', '$rootScope',
+    function(TB, $rootScope) {
+      var OTSession = {
         streams: [],
         connections: [],
         publishers: [],
-        init: function (apiKey, sessionId, token, cb) {
-            this.session = TB.initSession(sessionId);
+        init: function(apiKey, sessionId, token, cb) {
+          this.session = TB.initSession(sessionId);
 
-            OTSession.session.on({
-                sessionConnected: function(event) {
-                    OTSession.publishers.forEach(function (publisher) {
-                        OTSession.session.publish(publisher);
-                    });
-                },
-                streamCreated: function(event) {
-                    $rootScope.$apply(function() {
-                        OTSession.streams.push(event.stream);
-                    });
-                },
-                streamDestroyed: function(event) {
-                    $rootScope.$apply(function() {
-                        OTSession.streams.splice(OTSession.streams.indexOf(event.stream), 1);
-                    });
-                },
-                sessionDisconnected: function(event) {
-                    $rootScope.$apply(function() {
-                        OTSession.streams.splice(0, OTSession.streams.length);
-                    });
-                },
-                connectionCreated: function (event) {
-                    $rootScope.$apply(function() {
-                        OTSession.connections.push(event.connection);
-                    });
-                },
-                connectionDestroyed: function (event) {
-                    $rootScope.$apply(function() {
-                        OTSession.connections.splice(OTSession.connections.indexOf(event.connection), 1);
-                    });
-                }
-            });
+          OTSession.session.on({
+            sessionConnected: function() {
+              OTSession.publishers.forEach(function(publisher) {
+                OTSession.session.publish(publisher);
+              });
+            },
+            streamCreated: function(event) {
+              $rootScope.$apply(function() {
+                OTSession.streams.push(event.stream);
+              });
+            },
+            streamDestroyed: function(event) {
+              $rootScope.$apply(function() {
+                OTSession.streams.splice(OTSession.streams.indexOf(event.stream), 1);
+              });
+            },
+            sessionDisconnected: function() {
+              $rootScope.$apply(function() {
+                OTSession.streams.splice(0, OTSession.streams.length);
+              });
+            },
+            connectionCreated: function(event) {
+              $rootScope.$apply(function() {
+                OTSession.connections.push(event.connection);
+              });
+            },
+            connectionDestroyed: function(event) {
+              $rootScope.$apply(function() {
+                OTSession.connections.splice(OTSession.connections.indexOf(event.connection), 1);
+              });
+            }
+          });
 
-            this.session.connect(apiKey, token, function (err) {
-                if (cb) cb(err, OTSession.session);
-            });
-            this.trigger('init');
+          this.session.connect(apiKey, token, function(err) {
+            if (cb) cb(err, OTSession.session);
+          });
+          this.trigger('init');
         }
-    };
-    TB.$.eventing(OTSession);
-    return OTSession;
-}])
-.directive('otLayout', ['$window', '$parse', 'TB', 'OTSession', function($window, $parse, TB, OTSession) {
-    return {
+      };
+      TB.$.eventing(OTSession);
+      return OTSession;
+    }
+  ])
+  .directive('otLayout', ['$window', '$parse', 'TB', 'OTSession',
+    function($window, $parse, TB, OTSession) {
+      return {
         restrict: 'E',
         link: function(scope, element, attrs) {
-            var props = $parse(attrs.props)();
-            var container = TB.initLayoutContainer(element[0], props);
-            var layout = function () {
-              container.layout();
-              scope.$emit('otLayoutComplete');
-            };
-            scope.$watch(function() {
-                return element.children().length;
-            }, layout);
-            $window.addEventListener("resize", layout);
-            scope.$on("otLayout", layout);
-            var listenForStreamChange = function listenForStreamChange(session) {
-                OTSession.session.on("streamPropertyChanged", function (event) {
-                    if (event.changedProperty === 'videoDimensions') {
-                        layout();
-                    }
-                });
-            };
-            if (OTSession.session) listenForStreamChange();
-            else OTSession.on("init", listenForStreamChange);
+          var props = $parse(attrs.props)();
+          var container = TB.initLayoutContainer(element[0], props);
+          var layout = function() {
+            container.layout();
+            scope.$emit('otLayoutComplete');
+          };
+          scope.$watch(function() {
+            return element.children().length;
+          }, layout);
+          $window.addEventListener('resize', layout);
+          scope.$on('otLayout', layout);
+          var listenForStreamChange = function listenForStreamChange() {
+            OTSession.session.on('streamPropertyChanged', function(event) {
+              if (event.changedProperty === 'videoDimensions') {
+                layout();
+              }
+            });
+          };
+          if (OTSession.session) listenForStreamChange();
+          else OTSession.on('init', listenForStreamChange);
         }
-    };
-}])
-.directive('otPublisher', ['OTSession', function(OTSession) {
-    return {
+      };
+    }
+  ])
+  .directive('otPublisher', ['OTSession',
+    function(OTSession) {
+      return {
         restrict: 'E',
         scope: {
-            props: '&'
+          props: '&'
         },
-        link: function(scope, element, attrs){
-            var props = scope.props() || {};
-            props.width = props.width ? props.width : angular.element(element).width();
-            props.height = props.height ? props.height : angular.element(element).height();
-            var oldChildren = angular.element(element).children();
-            scope.publisher = TB.initPublisher(attrs.apikey || OTSession.session.apiKey,
-                element[0], props, function (err) {
-                if (err) {
-                    scope.$emit("otPublisherError", err, scope.publisher);
-                }
+        link: function(scope, element, attrs) {
+          var props = scope.props() || {};
+          props.width = props.width ? props.width : angular.element(element).width();
+          props.height = props.height ? props.height : angular.element(element).height();
+          var oldChildren = angular.element(element).children();
+          scope.publisher = TB.initPublisher(attrs.apikey || OTSession.session.apiKey,
+            element[0], props, function(err) {
+              if (err) {
+                scope.$emit('otPublisherError', err, scope.publisher);
+              }
             });
-            // Make transcluding work manually by putting the children back in there
-            angular.element(element).append(oldChildren);
-            scope.publisher.on({
-                accessDenied: function (event) {
-                    scope.$emit("otAccessDenied");
-                },
-                accessDialogOpened: function (event) {
-                    scope.$emit("otAccessDialogOpened");
-                },
-                accessDialogClosed: function(event){
-                    scope.$emit("otAccessDialogClosed");
-                },
-                accessAllowed: function(event) {
-                    angular.element(element).addClass("allowed");
-                    scope.$emit("otAccessAllowed");
-                },
-                loaded: function (event){
-                    scope.$emit("otLayout");
-                },
-                streamCreated: function (event) {
-                  scope.$emit("otStreamCreated");
-                },
-                streamDestroyed: function (event) {
-                  scope.$emit("otStreamDestroyed");
-                }
-            });
-            scope.$on("$destroy", function () {
-                if (OTSession.session) OTSession.session.unpublish(scope.publisher);
-                else scope.publisher.destroy();
-                OTSession.publishers = OTSession.publishers.filter(function (publisher) {
-                    return publisher !== scope.publisher;
-                });
-                scope.publisher = null;
-            });
-            if (OTSession.session && (OTSession.session.connected ||
-                    (OTSession.session.isConnected && OTSession.session.isConnected()))) {
-                OTSession.session.publish(scope.publisher, function (err) {
-                    if (err) {
-                        scope.$emit("otPublisherError", err, scope.publisher);
-                    }
-                });
+          // Make transcluding work manually by putting the children back in there
+          angular.element(element).append(oldChildren);
+          scope.publisher.on({
+            accessDenied: function() {
+              scope.$emit('otAccessDenied');
+            },
+            accessDialogOpened: function() {
+              scope.$emit('otAccessDialogOpened');
+            },
+            accessDialogClosed: function() {
+              scope.$emit('otAccessDialogClosed');
+            },
+            accessAllowed: function() {
+              angular.element(element).addClass('allowed');
+              scope.$emit('otAccessAllowed');
+            },
+            loaded: function() {
+              scope.$emit('otLayout');
+            },
+            streamCreated: function() {
+              scope.$emit('otStreamCreated');
+            },
+            streamDestroyed: function() {
+              scope.$emit('otStreamDestroyed');
             }
-            OTSession.publishers.push(scope.publisher);
+          });
+          scope.$on('$destroy', function() {
+            if (OTSession.session) OTSession.session.unpublish(scope.publisher);
+            else scope.publisher.destroy();
+            OTSession.publishers = OTSession.publishers.filter(function(publisher) {
+              return publisher !== scope.publisher;
+            });
+            scope.publisher = null;
+          });
+          if (OTSession.session && (OTSession.session.connected ||
+            (OTSession.session.isConnected && OTSession.session.isConnected()))) {
+            OTSession.session.publish(scope.publisher, function(err) {
+              if (err) {
+                scope.$emit('otPublisherError', err, scope.publisher);
+              }
+            });
+          }
+          OTSession.publishers.push(scope.publisher);
         }
-    };
-}])
-.directive('otSubscriber', ['OTSession', function(OTSession) {
-    return {
+      };
+    }
+  ])
+  .directive('otSubscriber', ['OTSession',
+    function(OTSession) {
+      return {
         restrict: 'E',
         scope: {
-            stream: '=',
-            props: '&'
+          stream: '=',
+          props: '&'
         },
-        link: function(scope, element, attrs){
-            var stream = scope.stream,
-                props = scope.props() || {};
-            props.width = props.width ? props.width : angular.element(element).width();
-            props.height = props.height ? props.height : angular.element(element).height();
-            var oldChildren = angular.element(element).children();
-            var subscriber = OTSession.session.subscribe(stream, element[0], props, function (err) {
-                if (err) {
-                    scope.$emit("otSubscriberError", err, subscriber);
-                }
-            });
-            subscriber.on("loaded", function () {
-                scope.$emit("otLayout");
-            });
-            // Make transcluding work manually by putting the children back in there
-            angular.element(element).append(oldChildren);
-            scope.$on("$destroy", function () {
-                OTSession.session.unsubscribe(subscriber);
-            });
+        link: function(scope, element) {
+          var stream = scope.stream,
+            props = scope.props() || {};
+          props.width = props.width ? props.width : angular.element(element).width();
+          props.height = props.height ? props.height : angular.element(element).height();
+          var oldChildren = angular.element(element).children();
+          var subscriber = OTSession.session.subscribe(stream, element[0], props, function(err) {
+            if (err) {
+              scope.$emit('otSubscriberError', err, subscriber);
+            }
+          });
+          subscriber.on('loaded', function() {
+            scope.$emit('otLayout');
+          });
+          // Make transcluding work manually by putting the children back in there
+          angular.element(element).append(oldChildren);
+          scope.$on('$destroy', function() {
+            OTSession.session.unsubscribe(subscriber);
+          });
         }
-    };
-}]);
+      };
+    }
+  ]);
